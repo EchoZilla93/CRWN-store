df0f8eaf0a5ec5780bdd0a8dbef3dd0d8510c9fc
EchoZilla93 <echozilla666@gmail.com>
2020-10-09 16:29:09 +0300

    shop page,collection-preview & collection-item component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,8 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 
 import HomePage from '../pages/homepage/HomePage';
+import ShopPage from '../pages/shop/ShopPage';
 import '../styles/App.scss';
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
 
 class App extends React.Component{
     render(){
@@ -16,7 +11,7 @@ class App extends React.Component{
             <div>
                 <Switch>
                     <Route exact path="/" component={HomePage}/>
-                    <Route  path="/hats" component={HatsPage}/>
+                    <Route  path="/shop" component={ShopPage}/>
                 </Switch>
             </div>
         );
---
+++ src/pages/shop/ShopPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {SHOP_DATA} from './shopData';
+import PrevCollection from '../../components/preview-collection/PrevCollection';
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    render(){
+        const {collections} = this.state;
+        return(
+            <div className="shop-page">
+                {
+                    collections.map(({id,...otherCollectionProps}) => {
+                        return (
+                            <PrevCollection 
+                            key={id} {...otherCollectionProps}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
