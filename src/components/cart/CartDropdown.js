babaebfb27dad660740c13baac7b3eb56d56a06d
EchoZilla93 <echozilla666@gmail.com>
2020-10-16 16:22:10 +0300

    rendering cart items

--- src/components/cart/CartDropdown.js
+++ src/components/cart/CartDropdown.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Button from '../button/Button';
+import CartItem from '../cart-item/CartItem';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
+            {
+                cartItems.map(cartItem => <CartItem 
+                    key={cartItem.id}
+                    item={cartItem}
+                    />)
+            }
+        </div>
             <Button>CHECKOUT</Button>
         </div>
-    </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({cart:{cartItems}}) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
